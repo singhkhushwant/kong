8855e7c5aa53058c5ab141ca44124ab5eb0582c4
samugi <samuele@konghq.com>
2024-08-22 17:49:02 +0200

    fix - otel utils

--- kong/plugins/opentelemetry/_handler.lua
+++ kong/plugins/opentelemetry/_handler.lua
@@ -1,6 +1,6 @@
 local otel_traces = require "kong.plugins.opentelemetry.traces"
 local otel_logs = require "kong.plugins.opentelemetry.logs"
-local utils = require "kong.tools.utils"
+local otel_utils = require "kong.plugins.opentelemetry.utils"
 local kong_meta = require "kong.meta"
 
 return function(priority)
@@ -16,11 +16,11 @@ return function(priority)
     if configs then
       for _, config in ipairs(configs) do
         if config.logs_endpoint then
-          utils.start_log_hooks()
+          otel_utils.start_log_hooks()
         end
 
         -- enable instrumentations based on the value of `config.tracing_instrumentations`
-        utils.start_instrumentation_hooks()
+        otel_utils.start_instrumentation_hooks()
       end
     end
   end
